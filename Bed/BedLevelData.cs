87ede9352c17adc75224e1d87ff06d9597d33aee
milemihailov <milemihailov5@gmail.com>
2023-09-25 09:43:12 +0200

    Rearanged classes

--- Bed/BedLevelData.cs
+++ Bed/BedLevelData.cs
@@ -1,7 +1,32 @@
 ﻿namespace ControllingAndManagingApp.Bed
 {
+
     public class BedLevelData
     {
+        public const int LINES_TO_EXCLUDE = 4;
+
         public string BedLevelGridData;
+
+
+        /// <summary>
+        /// Get's the data from autobedlevel "G29" command
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string GetGrid(string input)
+        {
+
+            char[] delimiters = new char[] { '\r', '\n' };
+
+            string[] splitedString = input.Split(delimiters);
+
+            string grid = "";
+
+            for (int i = 0; i < splitedString.Length - LINES_TO_EXCLUDE; i++)
+            {
+                grid += $"{splitedString[i]}\n";
+            }
+            return grid;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,46 +1,29 @@
-﻿using ControllingAndManagingApp.Gcode;
-using ControllingAndManagingApp.Material;
-using ControllingAndManagingApp.Motion;
+﻿using ControllingAndManagingApp.Motion;
 
 namespace ControllingAndManagingApp
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            GCodeCommands command = new GCodeCommands();
-            Position position = new Position();
-            MotionSettingsData motionSettingsData = new MotionSettingsData();
 
-            position.XMovePosition = 1;
-            position.YMovePosition = 2;
-            position.ZMovePosition = 0;
-            position.XHomePosition = 0;
-            position.YHomePosition = 3;
-            position.ZHomePosition = 2;
-            motionSettingsData.FeedRateFreeMove = 1;
-            Print.Print filename = new Print.Print();
-            filename.File = "fee";
+            SerialConnection.SerialConnection serialPort = new();
 
-            Temps.BedTemps hotendTemps = new Temps.BedTemps();
-            hotendTemps.BedSetTemp = 220;
+            serialPort.PortName = "COM4";
+            serialPort.BaudRate = 115200;
 
-            //motionSettingsData.FanSpeed = 200;
 
-            PreheatingProfiles profiles = new PreheatingProfiles();
-            profiles.HotendTemp = 200;
-            profiles.BedTemp = 300;
-            //profiles.FanSpeed = 32;
-            profiles.MaterialIndex = 2;
-            FilamentProperties fil = new FilamentProperties();
-            fil.FilamentDiameter = 1;
 
-            motionSettingsData.EMaxFeedrate = 1;
-            motionSettingsData.ZHomeOffset = 2;
+            serialPort.Open();
+            serialPort.Write(CommandMethods.SendFanOff());
+            serialPort.CheckForBusy();
 
-            Console.WriteLine(CommandMethods.HomeAxes(position));
+            string input = serialPort.Read();
 
-            Console.WriteLine(CommandMethods.DeleteSDFile(filename.File));
+            Console.WriteLine(input);
+
+            serialPort.Close();
         }
 
     }
