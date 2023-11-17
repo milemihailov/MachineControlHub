3542b2eebeb498e856c96f230c003df431b59b2e
milemihailov <milemihailov5@gmail.com>
2023-11-17 13:53:28 +0100

    Create WiFiConnection.cs

---
+++ PrinterConnection/WiFiConnection.cs
@@ -0,0 +1,35 @@
+﻿namespace ControllingAndManagingApp.PrinterConnection
+{
+    public class WiFiConnection : IPrinterConnection
+    {
+        public void Initialize(string connectionString)
+        {
+
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Disconnect()
+        {
+
+        }
+
+        public void Write(string message)
+        {
+
+        }
+
+        public string Read()
+        {
+            return null;
+        }
+
+        public List<string> AvailableConnections()
+        {
+            return new List<string>();
+        }
+    }
+}
