6b9d801a7f2309e627bbfe9c6bfab4894aa8effa
milemihailov <milemihailov5@gmail.com>
2023-12-09 22:01:46 +0100

    Update SerialConnectionService.cs

--- WebUIBlazor/Data/SerialConnectionService.cs
+++ WebUIBlazor/Data/SerialConnectionService.cs
@@ -5,33 +5,11 @@ namespace WebUI.Data
     public class SerialConnectionService
     {
 
-        readonly SerialConnection serialConnection = new();
-
-        public string portName = "";
-        public int baudRate;
-
-        
-        public void Connect()
-        {
-            serialConnection.PortName = portName;
-            serialConnection.BaudRate = baudRate;
-            serialConnection.Connect();
-            
-        }
-
-        public void Disconnect()
-        {
-            serialConnection.Disconnect();
-        }
+        public IPrinterConnection printerConnection;
 
         public void Write(string value)
         {
-            serialConnection.Write(value);
-        }
-
-        public List<string> AvailableConnections() 
-        {
-            return serialConnection.AvailableConnections();
+            printerConnection.Write(value);
         }
     }
 }
