5b3a17cc2feed628abcec25ed31c35d4b5649843
milemihailov <milemihailov5@gmail.com>
2023-12-22 14:38:31 +0100

    Create PrintingService.cs

---
+++ WebUIBlazor/Data/PrintingService.cs
@@ -0,0 +1,21 @@
+﻿using MachineControlHub.Print;
+
+namespace WebUI.Data
+{
+    public class PrintingService
+    {
+        public PrintService printService;
+        public PrintJob printJob;
+
+        public PrintingService()
+        {
+            printService = new PrintService(Data.ConnectionServiceSerial.printerConnection);
+            printJob = new PrintJob(Data.ConnectionServiceSerial.printerConnection);
+        }
+
+        public void SelectFile(string fileName)
+        {
+            printService.SelectPrint(fileName);
+        }
+    }
+}
