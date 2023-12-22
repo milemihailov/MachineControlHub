88c8643680e6d691ce974967f7b88351304975d9
milemihailov <milemihailov5@gmail.com>
2023-12-22 14:35:19 +0100

    Create BedTemperatureService.cs

---
+++ WebUIBlazor/Data/BedTemperatureService.cs
@@ -0,0 +1,23 @@
+﻿using MachineControlHub.Temps;
+
+namespace WebUI.Data
+{
+    public class BedTemperatureService
+    {
+        public BedTemps bed;
+        public BedTemperatureService() 
+        {
+            bed = new BedTemps(Data.ConnectionServiceSerial.printerConnection);
+        }
+
+        public void SetBedTemperature() 
+        {
+            bed.SetBedTemperature();
+        }
+
+        public void ParseCurrentBedTemperature()
+        {
+            bed.ParseCurrentTemperature();
+        }
+    }
+}
