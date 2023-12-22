87ef07f61a31e1f4b2c4389528ac9757b407959a
milemihailov <milemihailov5@gmail.com>
2023-12-22 14:35:24 +0100

    Create ConnectionServiceSerial.cs

---
+++ WebUIBlazor/Data/ConnectionServiceSerial.cs
@@ -0,0 +1,46 @@
+﻿using MachineControlHub.PrinterConnection;
+using MachineControlHub.Temps;
+using Microsoft.AspNetCore.Components;
+
+namespace WebUI.Data
+{
+    public class ConnectionServiceSerial
+    {
+
+        public static SerialConnection printerConnection;
+
+        public bool inititalized = false;
+
+
+        public ConnectionServiceSerial()
+        {
+            printerConnection = new SerialConnection();
+        }
+
+        public void Connect()
+        {
+            printerConnection.Connect();
+        }
+
+        public void Initialize(string connectionString)
+        {
+            printerConnection.Initialize(connectionString);
+        }
+
+        public void Write(string value)
+        {
+            printerConnection.Write(value);
+        }
+
+        public void Disconnect()
+        {
+            printerConnection.Disconnect();
+            inititalized = false;
+        }
+
+        public List<string> GetPorts()
+        {
+            return printerConnection.AvailableConnections();
+        }
+    }
+}
