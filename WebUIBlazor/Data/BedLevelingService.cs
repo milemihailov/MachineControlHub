fe0e00490722f279728796b8fea98b4a90ef411d
milemihailov <milemihailov5@gmail.com>
2024-01-18 02:26:26 +0100

    Create BedLevelingService.cs

---
+++ WebUIBlazor/Data/BedLevelingService.cs
@@ -0,0 +1,25 @@
+﻿using MachineControlHub.Bed;
+using MachineControlHub.Motion;
+using WebUI.Pages;
+
+namespace WebUI.Data
+{
+    public class BedLevelingService
+    {
+        public static BedLevelData bedData;
+        public string CSVData;
+
+        public BedLevelingService() 
+        {
+            bedData = new BedLevelData();
+        }
+
+        public void CalibrateBed()
+        {
+            Data.ConnectionServiceSerial.printerConnection.Write(CommandMethods.BuildBedLevelingCommand());
+            Data.ConnectionServiceSerial.printerConnection.CheckForBusy();
+            string input = Data.ConnectionServiceSerial.printerConnection.Read();
+            CSVData = bedData.GetGrid(input);
+        }
+    }
+}
